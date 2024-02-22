8fed3d7eac905e0a3608c4fda89b717e7d3f60db
shahchhatru <shahkhesavchhatra@gmail.com>
2024-02-22 14:18:25 +0545

    db connected

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from .database import engine
+from .models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app= FastAPI()
 
 @app.get('/')
 async def Home():
     return {"main":"Hello from the main world"}
+
+
